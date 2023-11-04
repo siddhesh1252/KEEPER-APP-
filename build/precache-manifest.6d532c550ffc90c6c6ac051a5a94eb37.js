b0c03c08119188efef54b9af75725d12a0d3daa9
unknown <siddheshw2002@gmail.com>
2023-11-04 15:00:37 +0530

    setup gh-pages

---
+++ build/precache-manifest.6d532c550ffc90c6c6ac051a5a94eb37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2367de8a7fb67d681a7547c94e2786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "42ba0359be000ed10995",
+    "url": "/static/js/main.18d60052.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
